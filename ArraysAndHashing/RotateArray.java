120eb1ad2337f6283dc6a388cd77210e516241a4
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-07-24 10:00:32 +0530

    rotating array by 90 degree bruteforce and optimal code added

---
+++ ArraysAndHashing/RotateArray.java
@@ -0,0 +1,44 @@
+package ArraysAndHashing;
+public class RotateArray {
+    static int[][] rotateBruteForce(int[][] matrix) { //T.C : O(n^2) S.C: O(n^2)
+        int n = matrix.length;
+        int rotated[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n-i-1] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+    static void rotateOptimal(int [][] matrix) { //T.C: O(n^2) S.C: O(1)
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length/2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        rotateOptimal(arr);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
