7d891d1b41c5a917ecf2632f31cf45d6b5107310
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-08-14 00:39:25 +0530

    add optimal soluton for union of array

--- Arrays_DSA_Questions/UnionOfArray.java
+++ Arrays_DSA_Questions/UnionOfArray.java
@@ -1,6 +1,8 @@
 package Arrays_DSA_Questions;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class UnionOfArray {
     public static void main(String[] args) {
@@ -11,8 +13,9 @@ public class UnionOfArray {
         for (int num : result) {
             System.out.print(num + " ");
         }
+        System.out.println();
+        System.out.println(unionOptimal(arr1, arr2));
     }
-
     static int[] union(int arr1[], int arr2[]) {
         HashSet<Integer> set = new HashSet<>();
 
@@ -31,5 +34,41 @@ public class UnionOfArray {
         }
 
         return unionArr;
+    }
+    static List<Integer> unionOptimal(int arr1[],int arr2[]) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        int i=0;
+        int j=0;
+
+        List<Integer> lst = new ArrayList<>();
+
+        while(i<n1 && j< n2) {
+            if(arr1[i] <= arr2[j]) {
+                if(lst.size() == 0 || !lst.contains(arr1[i])) {
+                    lst.add(arr1[i]);
+                }
+                i++;
+            }else{
+                if(lst.size() == 0 || !lst.contains(arr2[j])) {
+                    lst.add(arr2[j]);
+                }
+                j++;
+            }
+        }
+        while(i<n1) {
+            if(lst.size() == 0 || !lst.contains(arr1[i])) {
+                lst.add(arr1[i]);
+            }
+            i++;
+        }
+        while(j<n2) {
+            if(lst.size() == 0 || !lst.contains(arr2[j])) {
+                lst.add(arr2[j]);
+            }
+            j++;
+        }
+        return lst;
     }
 }
