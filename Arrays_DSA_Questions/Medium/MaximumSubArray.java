73b6fa3be28be88b6517bef55d8885ed1471c37a
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-08-18 23:21:18 +0530

    add code maximum subarray sum brute, better and optimal

---
+++ Arrays_DSA_Questions/Medium/MaximumSubArray.java
@@ -0,0 +1,57 @@
+package Arrays_DSA_Questions.Medium;
+
+public class MaximumSubArray {
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum1 = maxSubArraySumBrute(arr);
+        int maxSum2 = maxSubArraySumBetter(arr);
+        int maxSum3 = maxSubArraySumOptimal(arr);
+        System.out.println("The maximum subarray sum is: " + maxSum1);
+        System.out.println("The maximum subarray sum is: " + maxSum2);
+        System.out.println("The maximum subarray sum is: " + maxSum3);
+    }
+    static int maxSubArraySumBrute(int arr[]) { // T.C is O(n^3) and S.C is O(1)
+
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+    static int maxSubArraySumBetter(int arr[]) { // T.C is O(n^2) and S.C is O(1)
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+    static int maxSubArraySumOptimal(int arr[]) { // T.C is O(n) and S.C is O(1)
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (sum < 0) {
+                sum = 0;
+            }
+            if (sum > 0) {
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+}
