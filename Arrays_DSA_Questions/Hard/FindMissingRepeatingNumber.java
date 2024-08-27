599c1d06b5155cddb2d8dc46af0589bdf1c8564d
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-08-27 21:28:33 +0530

    add optimal solution for find missing and repeating number

--- Arrays_DSA_Questions/Hard/FindMissingRepeatingNumber.java
+++ Arrays_DSA_Questions/Hard/FindMissingRepeatingNumber.java
@@ -8,6 +8,8 @@ public class FindMissingRepeatingNumber {
         System.out.println("The repeating and missing numbers are: {"+ ans1[0] + ", " + ans1[1] + "}");
         int[] ans2 = findMissingRepeatingNumberBetter(a);
         System.out.println("The repeating and missing numbers are: {"+ ans2[0] + ", " + ans2[1] + "}");
+        int[] ans3 = findMissingRepeatingNumberOptimal_i(a);
+        System.out.println("The repeating and missing numbers are: {"+ ans3[0] + ", " + ans3[1] + "}");
     }
     static int[] findMissingRepeatingNumberBrute(int arr[]) {
         int n = arr.length;
@@ -46,5 +48,27 @@ public class FindMissingRepeatingNumber {
             else if(hash[i] == 2) repeating = i;
         }
         return new int[] {repeating, missing};
+    }
+    static int[] findMissingRepeatingNumberOptimal_i(int arr[]) {
+        int n = arr.length;
+        int sn = (n*(n+1)) / 2;
+        int s2n = (n * (n+1) * (2*n+1)) / 6;
+
+        int s = 0;
+        int s2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            s = s + arr[i];
+            s2 = s2 + arr[i] * arr[i];
+        }
+
+        int val1 = s - sn; //(x+y)
+        int val2 = s2 - s2n;
+
+        val2 = val2/val1; // (x-y)
+
+        int x = (val1 + val2) / 2;
+        int y = x - val1;
+        return new int[] {x,y};
     }
 }
