04ce99bd09922b72456e3d6727b24ee8a58db966
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-08-27 21:20:00 +0530

    add better solution for find missing and repeating

--- Arrays_DSA_Questions/Hard/FindMissingRepeatingNumber.java
+++ Arrays_DSA_Questions/Hard/FindMissingRepeatingNumber.java
@@ -4,8 +4,10 @@ package Arrays_DSA_Questions.Hard;
 public class FindMissingRepeatingNumber {
     public static void main(String[] args) {
         int[] a = {3, 1, 2, 5, 4, 6, 7, 5};
-        int[] ans = findMissingRepeatingNumberBrute(a);
-        System.out.println("The repeating and missing numbers are: {"+ ans[0] + ", " + ans[1] + "}");
+        int[] ans1 = findMissingRepeatingNumberBrute(a);
+        System.out.println("The repeating and missing numbers are: {"+ ans1[0] + ", " + ans1[1] + "}");
+        int[] ans2 = findMissingRepeatingNumberBetter(a);
+        System.out.println("The repeating and missing numbers are: {"+ ans2[0] + ", " + ans2[1] + "}");
     }
     static int[] findMissingRepeatingNumberBrute(int arr[]) {
         int n = arr.length;
@@ -26,6 +28,22 @@ public class FindMissingRepeatingNumber {
             if (repeating != -1 && missing != -1) {
                 break;
             }
+        }
+        return new int[] {repeating, missing};
+    }
+    static int[] findMissingRepeatingNumberBetter(int arr[]) {
+        int n = arr.length;
+        int hash[] = new int[n+1];
+
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]]++;
+        }
+        int repeating = -1;
+        int missing = -1;
+
+        for (int i = 1; i < hash.length; i++) {
+            if (hash[i] == 0) missing = i;
+            else if(hash[i] == 2) repeating = i;
         }
         return new int[] {repeating, missing};
     }
