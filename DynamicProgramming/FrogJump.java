26b0e1b648bc57cc6b0866d8de2bdd63e06ab6f0
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-02-07 23:59:43 +0530

    memoized solution for frog jump

--- DynamicProgramming/FrogJump.java
+++ DynamicProgramming/FrogJump.java
@@ -6,12 +6,19 @@ public class FrogJump {
         int n = heights.length;
         int minEnergy = findMinimumEnergyBrute(n - 1, heights);
         System.out.println("Minimum energy required: " + minEnergy);
+
+        int dp[] = new int[heights.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = -1;
+        }
+        int minEnergy2 = findMinimumEnergyMemoized(n - 1, heights,dp);
+        System.out.println("Minimum energy required: " + minEnergy2);
     }
     static int findMinimumEnergyBrute(int index, int arr[]) {
         if (index == 0) {
             return 0;
         }
-        int left = findMinimumEnergyBrute(index-1, arr) + Math.abs(arr[index] = arr[index-1]);
+        int left = findMinimumEnergyBrute(index-1, arr) + Math.abs(arr[index] - arr[index-1]);
 
         int right = Integer.MAX_VALUE;
 
@@ -19,5 +26,18 @@ public class FrogJump {
             right =  findMinimumEnergyBrute(index-2, arr) + Math.abs(arr[index] - arr[index - 2]);
         }
         return Math.min(left, right);
+    }
+    static int findMinimumEnergyMemoized(int index, int arr[], int dp[]) {
+        if (index == 0) return 0;
+        if (dp[index] != -1) return dp[index];
+        
+        int left = findMinimumEnergyMemoized(index - 1, arr,dp) + Math.abs (arr[index] - arr[index - 1]);
+        int right = Integer.MAX_VALUE;
+        if (index > 1) {
+            right = findMinimumEnergyMemoized(index - 2, arr, dp) + Math.abs(arr[index] - arr[index - 2]);
+        }
+
+        dp[index] = Math.min(left, right);
+        return dp[index];
     }
 }
