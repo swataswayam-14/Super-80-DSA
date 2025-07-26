b685bcf8977f72cfb08ffe7e09b848c44c46a50e
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-07-26 18:59:36 +0530

    mergeKLists divide approach

--- Top40/MergeKSortedList.java
+++ Top40/MergeKSortedList.java
@@ -63,4 +63,30 @@ public class MergeKSortedList {
         }
         return dummy.next;
     }
+    public static ListNode mergeKListsDivide(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        return mergeRange(lists, 0, lists.length - 1);
+    }
+    private static ListNode mergeRange(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = mergeRange(lists, left, mid);
+        ListNode l2 = mergeRange(lists, mid+1, right);
+        return mergeTwoLists(l1,l2);
+    }
+    private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), tail = dummy;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (l1 !=null) ? l1 : l2;
+        return dummy.next;
+    }
 }
