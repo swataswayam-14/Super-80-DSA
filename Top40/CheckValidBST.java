3255da351f9f2c5c78acd6e013331fdf30ece424
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-07-27 21:37:31 +0530

    validate BST

---
+++ Top40/CheckValidBST.java
@@ -0,0 +1,44 @@
+// brute force : 
+// find inorder and check if its strictly increasing
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckValidBST {
+    public static boolean isValidBSTBrute(TreeNode root) {
+        if(root == null) return true;
+        List<Integer> inorderTraversal = new ArrayList<>();
+        inorder(inorderTraversal, root);
+        for(int i=1;i< inorderTraversal.size(); i++) {
+            if(inorderTraversal.get(i) < inorderTraversal.get(i-1)) return false;
+        }
+        return true;
+    }
+    public static void inorder(List<Integer> inorderTraversal, TreeNode node) {
+        if(node == null) return;
+        inorder(inorderTraversal, node.left);
+        inorderTraversal.add(node.val);
+        inorder(inorderTraversal, node.right);
+    }
+    /*
+    Optimized: Min-Max Range Validation (DFS)
+    Idea:
+    Use recursion to ensure:
+
+    left < root < right
+
+    Maintain min and max constraints on each subtree
+
+    Time Complexity: O(n)
+    Space Complexity: O(h) (recursive call stack)
+     */
+    public static boolean isValidBSTOptmised(TreeNode root) {
+        return validate(root, null, null);
+    }
+    private static boolean validate(TreeNode node, Integer min, Integer max) {
+        if(node == null) return true;
+        if((min != null && node.val <= min) || (max != null && node.val >= max)) return false;
+
+        return validate(node.left, min, node.val) && validate(node.right, node.val, max);
+    }
+}
--- Top40/README.md
+++ Top40/README.md
@@ -22,7 +22,7 @@
 - [✅] 15. Same Tree
 - [✅] 16. Invert Binary Tree
 - [✅] 17. Binary Tree Level Order Traversal
-- [ ] 18. Validate Binary Search Tree
+- [✅] 18. Validate Binary Search Tree
 - [ ] 19. Lowest Common Ancestor of BST
 - [ ] 20. Binary Tree Right Side View
 - [ ] 21. Construct Binary Tree from Preorder and Inorder
