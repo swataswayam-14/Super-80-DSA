f832660948710152e24d392ec683a483342120f0
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-07-31 12:46:37 +0530

    optmised approach

--- GfG/PowerfulInteger.java
+++ GfG/PowerfulInteger.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class PowerfulInteger {
     public int powerfulInteger(int [][] intervals, int k) {
@@ -23,5 +24,26 @@ public class PowerfulInteger {
             }
         }
         return max == Integer.MIN_VALUE ? -1 : max;
+    }
+    public int powerfulIntegerOptmised(int [][] intervals, int k) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int[] interval: intervals) {
+            map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+            map.put(interval[1] + 1, map.getOrDefault(interval[1] + 1, 0) - 1);
+        }
+
+        int count = 0;
+        int max = -1;
+        int prev = -1;
+
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            int point = entry.getKey();
+            if(count >= k && prev != -1) {
+                max = Math.max(max, point - 1);
+            }
+            count += entry.getValue();
+            prev = point;
+        }
+        return max;
     }
 }
