a0f7799c63625266aa73f7b0d0e0e4bcdeecdd81
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-07-31 12:55:58 +0530

    check if b is subset of a

---
+++ GfG/ArrayProblems/ArraySubSet.java
@@ -0,0 +1,31 @@
+package ArrayProblems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArraySubSet {
+    public boolean isSubset(int a[], int b[]) {
+        if(b.length > a.length) return false;
+        //to find if b is a subset of a
+        Map<Integer, Integer> freq1 = new HashMap<>();
+        Map<Integer, Integer> freq2 = new HashMap<>();
+
+        for(int num: a) {
+            freq1.put(num, freq1.getOrDefault(num, 0) + 1);
+        }
+        for(int num: b) {
+            freq2.put(num, freq2.getOrDefault(num, 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry2: freq2.entrySet()) {
+            int num = entry2.getKey();
+            if(!freq1.containsKey(num)) return false;
+            else {
+                if(!(freq1.get(num) >= freq2.get(num))) { // the frequency of the element in subset array is more than that present in the parent array
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+}
