904da3ed9026e5991b2bcfb0e3f29dcec6a4d3d2
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-07-31 13:12:30 +0530

    slight optmisation

--- GfG/ArrayProblems/ArraySubSet.java
+++ GfG/ArrayProblems/ArraySubSet.java
@@ -1,5 +1,6 @@
 package ArrayProblems;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +26,23 @@ public class ArraySubSet {
                     return false; 
                 }
             }
+        }
+        return true;
+    }
+    public boolean isSubsetOptmised(int a[], int b[]) { // slight optmise
+        if(b.length > a.length) return false;
+        //to find if b is a subset of a
+        Map<Integer, Integer> freq1 = new HashMap<>();
+        Map<Integer, Integer> freq2 = new HashMap<>();
+
+        for(int num: a) {
+            freq1.put(num, freq1.getOrDefault(num, 0) + 1);
+        }
+        for(int num: b) {
+            freq2.put(num, freq2.getOrDefault(num, 0) + 1);
+        }
+        for(int key: freq2.keySet()) {
+            if(freq1.getOrDefault(key, 0) < freq2.get(key)) return false;
         }
         return true;
     }
