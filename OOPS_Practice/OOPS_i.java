e8aee7ac14eb61d1cf11980568da9f938017b10b
Swata Swayam Dash <swataswayamdash@gmail.com>
2025-08-10 19:45:27 +0530

    static and instance

--- OOPS_Practice/OOPS_i.java
+++ OOPS_Practice/OOPS_i.java
@@ -26,9 +26,33 @@ public class OOPS_i {
                 ((MotorCycle)vehicle).wheelie();
             }
         }
+
+        MathOperation addition = (a, b) -> a+b;
+        int sum = addition.operate(2, 3);
+
+        MathOperation multiplication = (a,b) -> a*b;
+        int product = multiplication.operate(5, 3);
+
+        addition.printResult(sum);
+        multiplication.printResult(product);
+
+
+        Employee.displayCompanyInfo();
+
+        Employee emp1 = new Employee("Billu", 60, 21000000);
+        Employee emp2 = new Employee("Harvey", 171, 18000000);
+
+        emp1.displayEmployeeInfo();;
+        emp2.displayEmployeeInfo();
+
     }
-
-
+    @FunctionalInterface
+    interface MathOperation {
+        int operate(int a, int b);
+        default void printResult(int result) {
+            System.out.println(result);
+        }
+    }
 }
 
 class Vehicle {
@@ -124,3 +148,75 @@ interface Drawable {
 }
 
 
+//overloading and overriding
+
+class Animal {
+    public void makeSound() {
+        System.out.println("Animal making a sound");
+    }
+    public void sleep() {
+        System.out.println("The animal is sleeping");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void makeSound() { //overriding
+        System.out.println("Cat is making a sound");
+    }
+
+    public void makeSound(String str) { //overloading
+        System.out.println("The cat is trying to make a sound " + str);
+    }
+}
+
+
+
+class Employee {
+    //static belongs to the class, shared among all the instances , 
+    //instance belong to specific object instance 
+    // static -> in method area
+    // instance -> in heap
+
+    private String name;
+    private int employeeId;
+    private double salary;
+
+    private static int totalEmployees = 0;
+    private static String companyName = "TechCorp";
+
+    static {
+        System.out.println("Employee class loaded!");
+        companyName = "TechCorp Inc.";
+    }
+
+    {
+        //instance block (executed before constructor)
+        totalEmployees++;
+        System.out.println("Creating a new employee");
+    }
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public void displayEmployeeInfo() {
+        System.out.println("name: "+name+ " ID: "+ employeeId+", salary: "+salary);
+        System.out.println("Company: "+companyName);
+    }
+
+    //static method (can only access static members directly)
+    public static void displayCompanyInfo() {
+        System.out.println("Company: "+companyName);
+        System.out.println("Total Employees: "+totalEmployees);
+    }
+
+    public static int getTotalEmployee() {
+        return totalEmployees;
+    }
+    public void companyWithTotal() {
+        System.out.println("This employee's salary: " + this.salary);
+        System.out.println("Total Employees: "+totalEmployees);
+    }
+}
